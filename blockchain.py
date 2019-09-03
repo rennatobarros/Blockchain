aea8d115dbba15b71ecce0c3205e9e2cec7d33bf
Victor Gomes De Menezes <victormenezez15@A307-34L.imd.local>
2019-09-03 18:28:50 -0300

    Inicia criação da função de mineração

--- blockchain.py
+++ blockchain.py
@@ -1,55 +1,96 @@
-import hashlib, json, time
+import hashlib, json, time, copy
+
+
+DIFFICULTY = 4 # Quantidade de zeros (em hex) iniciais no hash válido.
 
 
 class Blockchain(object):
 
-    def __init__(self):
-        self.chain = []
-        self.memPool = []
-        self.createGenesisBlock()
+	def __init__(self):
+		self.chain = []
+		self.memPool = []
+		self.createGenesisBlock()
 
-    def createGenesisBlock(self):
-    	self.createBlock()
 
-    def createBlock(self):
-    	if self.chain:
-    		previous_block = self.chain[-1]
-    		previous_block.pop('transactions')
-    		previousHash = self.generateHash(previous_block)
-    	else:
-    		previousHash = '0000000000000000000000000000000000000000000000000000000000000000' 
+	def createGenesisBlock(self):
+		self.createBlock()
 
-    	block = {
-    		'index': len(self.chain),
-    		'timestamp': int(time.time()),
-    		'nonce': 0,
-    		'merkleRoot': 0,
-    		'previousHash': previousHash,
-    		'transactions': []
-    	}
 
-    	self.chain.append(block)
+	def createBlock(self):
+		if self.chain:
+			previous_block = copy.deepcopy(self.chain[-1])
+			previous_block.pop('transactions')
+			previousHash = self.generateHash(previous_block)
+		else:
+			previousHash = '0000000000000000000000000000000000000000000000000000000000000000'
 
-    	return block
+		block = {
+			'index': len(self.chain),
+			'timestamp': int(time.time()),
+			'nonce': 0,
+			'merkleRoot': 0,
+			'previousHash': previousHash,
+			'transactions': []
+		}
 
-    @staticmethod
-    def generateHash(data):
-        blkSerial = json.dumps(data, sort_keys=True).encode()
-        return hashlib.sha256(blkSerial).hexdigest()
+		self.chain.append(block)
 
-    def printChain(self):
-    	for block in reversed(self.chain):
-    		# block.pop('transactions')
-    		selfhash = self.generateHash(block)
-    		print('===================================== %s° BLOCK ====================================' % block['index'])
-    		# print('Self Hash: ', selfhash)
-    		print('Timestamp: ', block['timestamp'])
-    		print('Nonce: ', block['nonce'])
-    		print('merkleRoot: ', block['merkleRoot'])
-    		print('previousHash: ', block['previousHash'])
-    		print('====================================================================================\n\n')
+		return block
+
+
+	def mineProofOfWork(self, prevBlock):
+
+
+
+		while True:
+			prevBlock['nonce'] += 1
+			prev_hash = Blockchain.generateHash(prevBlock)
+			
+			if 
+
+		pass
+
+
+	@staticmethod
+	def generateHash(data):
+		blkSerial = json.dumps(data, sort_keys=True).encode()
+		return hashlib.sha256(blkSerial).hexdigest()
+
+
+	@staticmethod
+	def getBlockID(block):
+		b = copy.deepcopy(block)
+		b.pop('transactions')
+		return Blockchain.generateHash(b)
+
+
+	@property
+	def prevBlock(self):
+		return self.chain[-1]
+
+
+	@staticmethod
+	def isValidProof(block, nonce):
+		
+
+
+	def printChain(self):
+		for block in reversed(self.chain):
+			selfhash = Blockchain.getBlockID(block)
+
+
+			print('\t\t\t\t\tA')
+			print('\t\t\t\t\t|\n')
+			print(' =================================== %s° BLOCK ==================================' % block['index'])
+			print('| Self Hash: ', selfhash)
+			print('| Timestamp: ', block['timestamp'])
+			print('| Nonce: ', block['nonce'])
+			print('| merkleRoot: ', block['merkleRoot'])
+			print('| previousHash: ', block['previousHash'])
+			print(' ================================================================================')
+
 
 # Teste
 blockchain = Blockchain()
 for x in range(0, 13): blockchain.createBlock()
-blockchain.printChain()
+# blockchain.printChain()
