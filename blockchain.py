f3dd2fd40069b082fdc15c227d538624657cb0e4
Victor Gomes <victormenezez15@gmail.com>
2019-10-21 17:50:36 -0300

    Começa isValidChain()

--- blockchain.py
+++ blockchain.py
@@ -7,7 +7,7 @@ import random
 from bitcoin.wallet import CBitcoinSecret
 from bitcoin.signmessage import BitcoinMessage, VerifyMessage, SignMessage
 
-DIFFICULTY = 3 # Quantidade de zeros (em hex) iniciais no hash válido.
+DIFFICULTY = 3 # Quantidade de zeros (em hex) iniciais no hash valido.
 
 class Blockchain(object):
 
@@ -16,9 +16,11 @@ class Blockchain(object):
         self.memPool = []
         self.createGenesisBlock()
 
+
     def createGenesisBlock(self):
         self.createBlock(previousHash='0'*64, nonce=0)
-        self.mineProofOfWork(self.prevBlock) 
+        self.mineProofOfWork(self.prevBlock)
+
 
     def createBlock(self, nonce=0, previousHash=None):
         if (previousHash == None):
@@ -26,18 +28,27 @@ class Blockchain(object):
             previousBlockCopy = copy.copy(previousBlock)
             previousBlockCopy.pop("transactions", None)
 
+        copy_mempool = copy.copy(self.memPool)
+
+        for i in range(len(copy_mempool)):
+            copy_mempool[i] = Blockchain.generateHash(copy_mempool[i])
+
+        print(copy_mempool)
+
         block = {
             'index': len(self.chain) + 1,
             'timestamp': int(time()),
-            'transactions': self.memPool,
-            'merkleRoot': self.generateMerkleRoot(self.memPool),
+            'transactions': '',
+            'merkleRoot': self.generateMerkleRoot(copy_mempool),
             'nonce': nonce,
             'previousHash': previousHash or self.generateHash(previousBlockCopy),
         }
 
         self.memPool = []
         self.chain.append(block)
+
         return block
+
 
     def mineProofOfWork(self, prevBlock):
         nonce = 0
@@ -45,6 +56,7 @@ class Blockchain(object):
             nonce += 1
 
         return nonce
+
 
     def createTransaction(self, sender, recipient, amount, timestamp, privKey):
         transaction = {
@@ -58,25 +70,27 @@ class Blockchain(object):
 
         self.memPool.append(transaction)
 
+
     @staticmethod
     def generateMerkleRoot(transactions):
         copy_transactions = []
 
-        for i in range(0,len(transactions)-1, 2):
-            if len(transactions) == 1:
-                return transactions[0]
+        if len(transactions) == 0:
+            return ''
+        elif len(transactions) == 1:
+            return transactions[0]
 
+        for i in range(0, len(transactions)-1, 2):
             first_hash = Blockchain.generateHash(transactions[i])
-            
+
             second_hash = ''
-            
+
             if not transactions[i+1]:
                 second_hash = first_hash
             else:
                 second_hash = Blockchain.generateHash(transactions[i+1])
 
             copy_transactions.append(Blockchain.generateHash(first_hash+second_hash))
-            print(len(copy_transactions))
 
         return Blockchain.generateMerkleRoot(copy_transactions)
 
@@ -85,18 +99,21 @@ class Blockchain(object):
     def isValidProof(block, nonce):
         block['nonce'] = nonce
         guessHash = Blockchain.getBlockID(block)
-        return guessHash[:DIFFICULTY] == '0' * DIFFICULTY 
+        return guessHash[:DIFFICULTY] == '0' * DIFFICULTY
+
 
     @staticmethod
     def generateHash(data):
         blkSerial = json.dumps(data, sort_keys=True).encode()
         return hashlib.sha256(blkSerial).hexdigest()
 
+
     @staticmethod
     def getBlockID(block):
         blockCopy = copy.copy(block)
         blockCopy.pop("transactions", None)
         return Blockchain.generateHash(blockCopy)
+
 
     def printChain(self):
         for block in reversed(self.chain):
@@ -105,7 +122,7 @@ class Blockchain(object):
             print(hex(int(selfhash, 16)))
             print('\t\t\t\t\tA')
             print('\t\t\t\t\t|\n')
-            print(' =================================== %s° BLOCK ==================================' % block['index'])
+            print(' =================================== %s BLOCK ==================================' % block['index'])
             print('| Self Hash: ', selfhash)
             print('| Timestamp: ', block['timestamp'])
             print('| Nonce: ', block['nonce'])
@@ -113,20 +130,53 @@ class Blockchain(object):
             print('| previousHash: ', block['previousHash'])
             print(' ================================================================================')
 
+
     @property
     def prevBlock(self):
         return self.chain[-1]
+
 
     @staticmethod
     def sign(privKey, message):
         secret = CBitcoinSecret(privKey)
         msg = BitcoinMessage(message)
         return SignMessage(secret, msg)
-        
+
+
     @staticmethod
     def verifySignature(address, signature, message):
         msg = BitcoinMessage(message)
         return VerifyMessage(address, msg, signature)
+
+
+    def isValidChain(self, chain):
+        for i, block in enumerate(chain):
+            prev_hash = ''
+
+            if i != 0:
+                header_prev_hash = block['previousHash']
+                prev_block = copy.copy(chain[i-1])
+                prev_block.pop('transactions')
+
+                block_prev_hash = Blockchain.generateHash()
+
+                if header_prev_hash != block_prev_hash:
+                    print(header_prev_hash)
+                    print(block_prev_hash)
+                    print("Bloco invalido! Hash do block invalido")
+                    return False
+
+            block_merkle_root = block['merkleRoot']
+
+            transactions_merkle_root = Blockchain.generateMerkleRoot(block['transactions'])
+
+            if block_merkle_root != transactions_merkle_root:
+                print(block_merkle_root)
+                print(transactions_merkle_root)
+                print("Bloco invalido! merkleRoot invalido.")
+                return False
+
+
 
 
 # Teste
@@ -135,9 +185,9 @@ blockchain = Blockchain()
 sender = '19sXoSbfcQD9K66f5hwP5vLwsaRyKLPgXF'
 recipient = '1MxTkeEP2PmHSMze5tUZ1hAV3YTKu2Gh1N'
 
-# Cria 5 blocos, incluindo o Genesis, contendo de 1-4 transações cada, com valores aleatórios, entre os endereços indicados em sender e recipient.
-for x in range(0, 4): 
-    for y in range(0, random.randint(1,4)) : 
+# Cria 5 blocos, incluindo o Genesis, contendo de 1-4 transacoes cada, com valores aleatorios, entre os enderecos indicados em sender e recipient.
+for x in range(0, 4):
+    for y in range(0, random.randint(1,4)):
         timestamp = int(time())
         amount = random.uniform(0.00000001, 100)
         blockchain.createTransaction(sender, recipient, amount, timestamp, 'L1US57sChKZeyXrev9q7tFm2dgA2ktJe2NP3xzXRv6wizom5MN1U')
